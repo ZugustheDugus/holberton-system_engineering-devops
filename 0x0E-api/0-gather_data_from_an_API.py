9e0f575cc637998c33324391da550e78820ad1b7
ZugustheDugus <83614659+ZugustheDugus@users.noreply.github.com>
2022-03-24 17:15:26 -0500

    asdf

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -2,24 +2,29 @@
 """
 Gather Data from an API
 """
+
+
 import requests
 from sys import argv
-
-if __name__ == "__main__":
+def todo():
 
     id = argv[1]
-    usr = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-    .format(id)).json()
+    usr = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(id)).json()
 
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
-    .format(id)).json()
+    to_do = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(id)).json()
 
     complete = []
 
-    for task in todos:
+    for task in to_do:
         if task.get("completed") is True:
             complete.append(task.get("title"))
-        print("Employee {} is done with tasks({}/{}):"
-            .format(usr.get('name'), len(complete), len(todos)))
-        for task in complete:
-            print("\t {}".format(task))
+    print("Employee {} is done with tasks({}/{}):"
+            .format(usr.get('name'), len(complete), len(to_do)))
+    for task in complete:
+        print("\t {}".format(task))
+
+
+if __name__ == '__main__':
+    todo()
