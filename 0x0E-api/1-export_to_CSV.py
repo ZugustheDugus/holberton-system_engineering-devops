642dc2f77528544fec15dcee1d634877fede3875
ZugustheDugus <h1cooper2019@protonmail.com>
2022-03-24 17:02:35 -0700

    pycodestyle

--- 0x0E-api/1-export_to_CSV.py
+++ 0x0E-api/1-export_to_CSV.py
@@ -8,19 +8,21 @@ import requests
 import csv
 from sys import argv
 
+
 def CSV():
     id = argv[1]
     usr = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-    .format(id)).json()
+                       .format(id)).json()
 
     todos = requests.get("https://jsonlaceholder.typicode.com/todos?userId={}"
-    .format(id)).json()
+                         .format(id)).json()
 
     with open("{}.csv".format(id), "w", newline="") as csvFile:
         wrot = csv.writer(csvFile, quotes=csv.QUOTE_ALL)
     for task in todos:
         wrot.writerow([int(id), usr.get("username"),
-                        task.get("completed"), task.get("title")])
+                       task.get("completed"), task.get("title")])
+
 
 if __name__ == '__main__':
     CSV()
