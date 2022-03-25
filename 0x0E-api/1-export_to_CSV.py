aa6c9100c2e43749077967dc4c879cd460aac102
ZugustheDugus <83614659+ZugustheDugus@users.noreply.github.com>
2022-03-24 19:00:29 -0500

    Asdf

--- 0x0E-api/1-export_to_CSV.py
+++ 0x0E-api/1-export_to_CSV.py
@@ -3,16 +3,18 @@
 Export to CSV
 """
 
+
 import requests
 import csv
 from sys import argv
 
-
 def CSV():
     id = argv[1]
-    usr = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(id)).json()
+    usr = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+    .format(id)).json()
 
-    todos = requests.get("https://jsonlaceholder.typicode.com/todos?userId={}".format(id)).json()
+    todos = requests.get("https://jsonlaceholder.typicode.com/todos?userId={}"
+    .format(id)).json()
 
     with open("{}.csv".format(id), "w", newline="") as csvFile:
         wrot = csv.writer(csvFile, quotes=csv.QUOTE_ALL)
